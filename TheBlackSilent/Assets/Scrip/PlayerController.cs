aa77845cf455622453b4aa22e9f00b536ce14d76
BurapaNG <burapa.ngam@bumail.net>
2025-10-02 02:46:14 +0700

    Add HideInLocked

--- TheBlackSilent/Assets/Scrip/PlayerController.cs
+++ TheBlackSilent/Assets/Scrip/PlayerController.cs
@@ -1,17 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private SpriteRenderer rend;
+    private bool canHide = false;
+    private bool hiding = false;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        rend = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        body.linearVelocity =new Vector2(Input.GetAxis("Horizontal") * speed,body.linearVelocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (canHide && Input.GetKeyDown(KeyCode.F))
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+            rend.sortingOrder = 0;
+            hiding = true;
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, false);
+            rend.sortingOrder = 2;
+            hiding = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (!hiding)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        }
+        else
+        {
+            body.velocity = Vector2.zero;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("locked"))
+        {
+            canHide = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("locked"))
+        {
+            canHide = false;
+        }
     }
 }
