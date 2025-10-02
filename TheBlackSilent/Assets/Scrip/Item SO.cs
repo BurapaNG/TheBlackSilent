4bd3e6ffec6820c4af01f3546d758e0cef14babe
D4Saki <thkvtpa@gmail.com>
2025-10-03 02:51:04 +0700

    Item SO and some change

---
+++ TheBlackSilent/Assets/Scrip/Item SO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    public StatToChange statToChange = new StatToChange();
+    public int amountToChangeStat;
+
+
+
+
+
+
+    public enum StatToChange
+    {
+        Battery,
+    };
+}
