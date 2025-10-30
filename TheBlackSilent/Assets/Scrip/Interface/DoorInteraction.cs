3b3a002e073b3682e6b3942210bf65e6b7bf94b9
BurapaNG <burapa.ngam@bumail.net>
2025-10-30 21:22:47 +0700

    Add UI Open Door

---
+++ TheBlackSilent/Assets/Scrip/Interface/DoorInteraction.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public TextMeshProUGUI interactionText;
+    public KeyCode interactionKey = KeyCode.F;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (interactionText != null)
+            {
+                interactionText.gameObject.SetActive(true);
+                interactionText.text = "Press " + interactionKey.ToString() + " to open";
+            }
+        }
+    }
+
+    // ส่วนนี้จะถูกเรียกเมื่อวัตถุอื่นออกจากระยะ
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // ปิดใช้งาน Text UI และซ่อนข้อความ
+            if (interactionText != null)
+            {
+                interactionText.gameObject.SetActive(false);
+            }
+        }
+    }
+}
