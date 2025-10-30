60e326a4269ceb849a88de2e302435475cc681ac
BurapaNG <burapa.ngam@bumail.net>
2025-10-30 21:48:14 +0700

    Add Audio Background
    เพิ่มโค้ดที่ใช้ควบคุมเสียง

---
+++ TheBlackSilent/Assets/Scrip/Audio/AudioScrips.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AudioScrips : MonoBehaviour
+{
+    [Header("--- Audio Source ---")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--- Audio Clip ---")]
+    public AudioClip backGround;
+    public AudioClip hide;
+    public AudioClip openDoor;
+
+    private void Start()
+    {
+        musicSource.clip = backGround;
+        musicSource.Play();
+    }
+}
