a43040a8b0ed44f354c9796e3319bb4ef5a432ea
Ritsutaka <o0skygamerch0o@gmail.com>
2025-10-31 02:47:21 -0700

    UI option Menu!!!
    ทำตรงส่วนของเกม Game UI เสร็จเรียบร้อยแล้ว

---
+++ TheBlackSilent/Assets/Scrip/Interface/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    public static bool isPauseMenu = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            Debug.Log("Pass ESC!!!");
+
+            if (isPauseMenu)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPauseMenu = false;
+        // Cursor.lockState = CursorLockMode.Locked;
+        // โค้ดด้านบนไว้ล็อคเมาส์
+    }
+
+    void Pause() 
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPauseMenu = true;
+        // Cursor.lockState = CursorLockMode.None
+        // โค้ดด้านบนไว้ปลดเมาส์
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+
+    public void LoadMenu() 
+    {
+        Time.timeScale = 1f;
+        // SceneManager.LoadScene("MainMenu");
+    }
+}
